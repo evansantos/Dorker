e410aba456feba2d7ae0e81b5fac7b5c214470c7
Evandro Santos <evan.its.me@gmail.com>
2020-06-15 20:38:45 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello, Learn React
         </a>
       </header>
     </div>
